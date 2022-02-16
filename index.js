0d3953aaff9e5948a0d02b5c54e37c5d825e2de0
Nicholas Krilis <n.krilis@gmail.com>
2022-02-15 21:32:22 -0500

    Created html functions and inquirer questions

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 
-// Refrence to html string
-const webStr = require('./src/helper');
+// Refrence to html creation functions
+const { createCard, starterHtml, closeHtml } = require('./src/helper');
 
 // Using inquirer for user input
 const inquirer = require('inquirer');
@@ -15,3 +15,114 @@ const inquirer = require('inquirer');
 // Using fs for file writing
 const fs = require("fs");
 
+function writeToFile() 
+{
+    let content = starterHtml();
+    fs.writeFile("./dist/index.html", content, (err) =>
+    err ? console.log(err) : console.log('Success! Your page was created'))
+}
+
+function appendCard(obj)
+{
+    let content = createCard(obj);
+    fs.appendFile("./dist/index.html", content, (err) =>
+    err ? console.log(err) : console.log('Success! Your page was created'))
+}
+
+function closeFile ()
+{
+    let content = closeHtml();
+    fs.appendFile("./dist/index.html", content, (err) =>
+    err ? console.log(err) : console.log('Success! Your page was created'))
+}
+
+const createEmployee = () =>
+{
+    inquirer
+        .prompt([
+        {
+            type: "list",
+            name: "type",
+            message: "Please select what kind of employee you would like",
+            choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the Employee's Name"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the Employee's ID number"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the Employee's Email"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter the Engineer's Github user name",
+            when(answers) {
+            return answers.type === "Engineer"
+            }
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Enter the Inter's School name",
+            when(answers) {
+                return answers.type === "Intern"
+            }
+        },
+        {
+            type: "input",
+            name: "officeNum",
+            message: "Enter the Managers Office Number",
+            when(answers) {
+                return answers.type === "Manager"
+            }
+        },
+        {
+            type: "list",
+            name: "more",
+            message: "Would you like to add another employee?",
+            choices: ['Yes', 'No']
+        }
+    ])
+    .then((data) =>
+    {
+        if(data.type === "Manager")
+        {
+            let manager = new Manager(data.name, data.id, data.email, data.officeNum);
+            appendCard(manager);
+        }
+        else if(data.type === "Engineer")
+        {
+            let engineer = new Engineer(data.name, data.id, data.email, data.github);
+            appendCard(engineer);
+        }
+        else if(data.type === "Intern")
+        {
+            let intern = new Intern(data.name, data.id, data.email, data.school);
+            appendCard(intern);
+        }
+        
+        if(data.more === "Yes")
+        {
+            return createEmployee();
+        }
+        else if(data.more === "No")
+        {
+            closeFile();
+        }
+    })
+    .catch((err) => 
+    {
+        console.log(err);
+    });
+}
+writeToFile();
+createEmployee();
--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,106 @@
-// --- Helper js to hold html string ---
+// --- Helper js to hold functions ---
 
-const webStr = `hello `;
+function starterHtml ()
+{
+  return `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-module.exports = webStr;
+      <!-- Bootstrap CSS -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+      <title>Team Profiles</title>
+    </head>
+    <body>
+      <header class="container-fluid bg-danger p-5 mb-5">
+          <div class="text-center">
+              <h1 class="text-white">My Team</h1>
+          </div>
+      </header>
+
+      <section class="container d-flex justify-content-center" id="team">
+      `;
+}
+
+function closeHtml ()
+{
+  return `
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+  </html>
+  `;
+}
+
+
+function createCard(obj)
+{
+  if(obj.getRole() === 'Manager')
+  {
+    return `
+    <div class="card m-2 h-100" style="width: 17rem;">
+            <div class="card-header text-white bg-primary">
+                <h3>${obj.getName()}</h3>
+                <h5><i class="bi bi-cup-fill"></i>  ${obj.getRole()}</h5>
+            </div>
+            <div class="card-body bg-light">
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${obj.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+                <li class="list-group-item">Office Number: ${obj.officeNumber} </li>
+              </ul>
+            </div>
+        </div>
+        `;
+  }
+
+  else if(obj.getRole() === 'Engineer')
+  {
+    return `
+    <div class="card m-2 h-100" style="width: 17rem;">
+            <div class="card-header text-white bg-primary">
+                <h3>${obj.getName()}</h3>
+                <h5><i class="bi bi-eyeglasses"></i>  ${obj.getRole()}</h5>
+            </div>
+            <div class="card-body bg-light">
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${obj.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a></li>
+                <li class="list-group-item">Github: <a href="https://github.com/${obj.getGithub()}">${obj.getGithub()}</a> </li>
+              </ul>
+            </div>
+        </div>
+        `;
+  }
+  
+  else if(obj.getRole() === 'Intern')
+  {
+    return `
+    <div class="card m-2 h-100" style="width: 17rem;">
+            <div class="card-header text-white bg-primary">
+                <h3>${obj.getName()}</h3>
+                <h5><i class="bi bi-mortarboard-fill"></i>  ${obj.getRole()}</h5>
+            </div>
+            <div class="card-body bg-light">
+              <ul class="list-group">
+                <li class="list-group-item">ID: ${obj.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${obj.getEmail()}">${obj.getEmail()}</a> </li>
+                <li class="list-group-item">School: ${obj.getSchool()} </li>
+              </ul>
+            </div>
+        </div>
+        `;
+  }
+}
+
+module.exports = 
+{
+  createCard,
+  starterHtml,
+  closeHtml,
+}
